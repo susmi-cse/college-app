98178b0b6890df5c7972026077e5b6104603905b
susmi-cse <susmitapo1234@gmail.com>
2026-01-12 00:06:48 +0530

    Fixed database connection pool

--- backend/db_config.js
+++ backend/db_config.js
@@ -1,24 +1,21 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 4000, 
     ssl: {
         minVersion: 'TLSv1.2',
         rejectUnauthorized: true
-    }
-});
-
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to Cloud DB:', err);
-    } else {
-        console.log('Connected to TiDB Cloud successfully!');
-    }
+    },
+    
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = db;
